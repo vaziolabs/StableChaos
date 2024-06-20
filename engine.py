906f8c2975b9fcd379cea99d5941ff7cbe08582b
alephpt <alephpt@github.com>
2024-06-20 16:26:03 -0500

    interpolation:

--- engine.py
+++ engine.py
@@ -5,10 +5,18 @@ from color import Color
 
 import sys
 
+def lerp(c_A, c_B, t):
+    t = (t + 2.0) / 4.0 # This value is potentially -2 to 2
+    return int(c_A + (c_B - c_A) * t)
+
 # Define Pygame Engine class
 class Engine:
     def __init__(self, screen_size, nodes):
         self.nodes = nodes
+        self.screen_size = screen_size
+        self.prev_r = 0
+        self.prev_g = 0
+        self.prev_b = 0
 
         # Initialize Pygame
         pygame.init()
@@ -20,6 +28,30 @@ class Engine:
         self.background = self.background.convert()
         self.background.fill(Color.WHITE_BACKGROUND.val())
         self.clock = pygame.time.Clock()
+            
+    def acumen(self):
+        init_color = Color.WHITE_FILL.val()
+        min_r, min_g, min_b = init_color
+        max_r, max_g, max_b = Color.BLACK_FILL.val()
+
+        for node in self.nodes:
+            r, g, b = Color.fromState(node.state)
+            print("comparing", r, g, b)
+            min_r = min(min_r, r)
+            min_g = min(min_g, g)
+            min_b = min(min_b, b)
+            max_r = max(max_r, r)
+            max_g = max(max_g, g)
+            max_b = max(max_b, b)
+
+        r, g, b = (lerp(min_r, max_r, 0.5), lerp(min_g, max_g, 0.5), lerp(min_b, max_b, 0.5))
+        new_r = (lerp(self.prev_r, r, 0.5) + 128) // 2
+        new_g = (lerp(self.prev_g, g, 0.5) + 128) // 2
+        new_b = (lerp(self.prev_b, b, 0.5) + 128) // 2
+        new_rgb = (new_r, new_g, new_b)
+        self.prev_r, self.prev_g, self.prev_b = new_rgb
+        return new_rgb
+
 
     def drawNodes(self):
         # Draw the nodes
@@ -53,13 +85,12 @@ class Engine:
             # Draw the background
             self.screen.blit(self.background, (0, 0))
 
-            self.calculateNodeValues()
+            # We draw a bigger circle to encompass all the nodes
+            circle_rad = self.screen_size[0] // 2 if self.screen_size[0] < self.screen_size[1] else self.screen_size[1] // 2.2
+            pygame.draw.circle(self.screen, self.acumen(), (800, 600), circle_rad)
 
-            # Draw the nodes
+            self.calculateNodeValues()
             self.drawNodes()
 
-            # Update the screen
             pygame.display.flip()
-
-            # Cap the frame rate
-            self.clock.tick(1000)
+            self.clock.tick(30)
--- node.py
+++ node.py
@@ -40,13 +40,13 @@ class Node:
         self.color = color
         self.radius = 50
         self.outline = 2
-        self.randomA = random.randint(-1, 1)
-        self.randomB = random.randint(-1, 1)
+        self.randomA = random.uniform(-1.0, 1.0)
+        self.randomB = random.uniform(-1.0, 1.0)
         self.state = State(self.randomA, self.randomB)
 
     def draw (self, screen):
         pygame.draw.circle(screen, Color.BLACK_OUTLINE.value, (self.position.x, self.position.y), self.radius + self.outline)
-        pygame.draw.circle(screen, Color.fromState(self.state).value, (self.position.x, self.position.y), self.radius)
+        pygame.draw.circle(screen, Color.fromState(self.state), (self.position.x, self.position.y), self.radius)
 
     def calculateNewRandomness(self):
         # Choose a random value between -1 and 1
@@ -70,45 +70,41 @@ class Node:
                 return
 
             if self.randomA > 0 and self.randomB > 0:
-                # If we have a double positive, we want to attempt to overtake or be like our neighbor
-                if node.state.A < 1:
-                    self.state.addA()
-                if node.state.B < 1:
-                    self.state.addB()
+                # If we have a double positive, we want to attempt to move away from our neighbor
+                self.state.minusA()
+                self.state.minusB()
                 return
             elif self.randomA < 0 and self.randomB < 0:
-                # If we have a double negative, we want to attempt to move away from our neighbor
-                if node.state.A > -1:
-                    self.state.minusA()
-                if node.state.B > -1:
-                    self.state.minusB()
+                # If we have a double negative, we want to attempt to overtake or be like our neighbor
+                self.state.addA()
+                self.state.addB()
                 return
             
             # Otherwise, we want to introduce some randomness to the node corresponding to our neighbor
             # TODO: Add a Gradient here
             if node.name is LeftNeighbor(self.name):
                 # If we have a mix of positive and negative, we want to attempt to be like our neighbor in one way or another
-                if self.randomA > 0: # If Random A is A Positive, we want to do LIKE our left neighbor in the A direction
-                    if node.state.A < 1:
+                if self.randomA > 0.0: # If Random A is A Positive, we want to do LIKE our left neighbor in the A direction
+                    if node.state.A < 1.0:
                         self.state.minusA()
-                    if node.state.B > -1:
+                    if node.state.B > -1.0:
                         self.state.minusB()
                 elif self.randomA < 0: 
-                    if node.state.A > -1:
+                    if node.state.A > -1.0:
                         self.state.addA()
-                    if node.state.B < 1:
+                    if node.state.B < 1.0:
                         self.state.addB()
                 return
             
             if node.name is RightNeighbor(self.name):
-                if self.randomB > 0: # If Random B is B Positive, we want to do LIKE our right neighbor in the B direction
-                    if node.state.B > -1:
+                if self.randomB > 0.0: # If Random B is B Positive, we want to do LIKE our right neighbor in the B direction
+                    if node.state.B > -1.0:
                         self.state.addB()
-                    if node.state.A < 1:
+                    if node.state.A < 1.0:
                         self.state.addA()
-                elif self.randomB < 0:
-                    if node.state.B < 1:
+                elif self.randomB < 0.0:
+                    if node.state.B < 1.0:
                         self.state.minusB()
-                    if node.state.A > -1:
+                    if node.state.A > -1.0:
                         self.state.minusA()
                 return
