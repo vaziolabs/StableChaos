454b3413f8c48345bd1ae6353376c57e402c90c7
alephpt <alephpt@github.com>
2024-06-23 00:26:08 -0500

    I think that our transceiver is correct

--- Tranception/tc_engine.py
+++ Tranception/tc_engine.py
@@ -9,5 +9,5 @@ class TCEngine(Engine):
         super().__init__(screen_size, [])
         self.evokation = Tranception(100)
 
-    # asyncio.create_task(node_list[0].rcv(440))
-    # asyncio.create_task(node_list[0].snd(node_list[1]))
+    def activate(self):
+        asyncio.run(self.evokation.induce())
--- Tranception/tranception.py
+++ Tranception/tranception.py
@@ -2,6 +2,8 @@
 from node import Node
 import asyncio
 
+sample_frequency = 44100
+
 # This is the parent class that enacts the tranception process
 #  which is synchronous and asynchronous in nature
 class Tranception:
@@ -20,13 +22,14 @@ class Tranception:
             for i in range(self.size) \
                 for j in range(1, self.size)]
 
-
+    # We are using the verbiage 'reflections' here to express the way that all of the 
+    # nodes are responding to a single timesteps 'inception' of a signal (440 in this case)
     async def induce(self):
         print("Evoker Inducing Tranception")
         print("")
         print(" - creating reflections")
         reflections = [asyncio.create_task(self.reflectors[0].incept())]
-        reflections += [asyncio.create_task(self.reflectors[0].snd(self.reflectors[i])) for i in range(1, 4)]
+        reflections += [asyncio.create_task(self.reflectors[0].snd(self.reflectors[i])) for i in range(1, self.size)]
         print(" - reflections created. awaiting...")
         print("") 
         return await asyncio.gather(*reflections)
