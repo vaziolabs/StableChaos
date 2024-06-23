e5ec13af880e73307593e533506834c73dbf316f
alephpt <alephpt@github.com>
2024-06-22 23:45:56 -0500

    engine abstractions mostly there

---
+++ Tranception/tc_test.py
@@ -0,0 +1,21 @@
+import asyncio
+from tc_engine import TCEngine
+
+# TODO: 
+#       Graph single node - single phase.. 
+#       Scale to multiple nodes simultaneously
+#       Color Code the nodes via the Engine
+#       Build Path and Particle System
+#       Add Gravity/Induction Cross Analysis
+#       Abstract the Transceiver functions away from the Node, and into Transmission
+#       Integrate with Nova and risc-v via SKIFT
+
+
+def main():
+    print(" ~~~~  Tranception dude!  ~~~~~")
+
+    engine = TCEngine((1600, 1200))
+    engine.run(print("Running Tranception Engine"))
+
+if __name__ == '__main__':
+    main()
--- Tranception/tranception.py
+++ Tranception/tranception.py
@@ -2,20 +2,24 @@
 from node import Node
 import asyncio
 
+# This is the parent class that enacts the tranception process
+#  which is synchronous and asynchronous in nature
 class Tranception:
-    def __init__(self):
+    def __init__(self, size):
+        self.size = size
         self.model = None # We don't know what this looks like 
         self.tokenizer = None # DONT: This is laughable (at least here)
         self.device = None # TODO: Integrate with Nova
         # Ignore these, they are conceptual
-        self.reflectors = [Node(i) for i in range(4)]
-        self.reflect()
+        self.reflectors = [Node(i) for i in range(size)]
+        self.reflect() #punnyhashtags
 
+    ## This function simply 'zips' all of our transceivers connections together through the nodes
     def reflect(self):
-        [self.reflectors[i].transceivers.add(self.reflectors[(i + j) % 4]) \
-            for i in range(4) \
-                for j in range(1, 4)]
-    
+        [self.reflectors[i].connections.add(self.reflectors[(i + j) % 4]) \
+            for i in range(self.size) \
+                for j in range(1, self.size)]
+
 
     async def induce(self):
         print("Evoker Inducing Tranception")
@@ -24,5 +28,5 @@ class Tranception:
         reflections = [asyncio.create_task(self.reflectors[0].incept())]
         reflections += [asyncio.create_task(self.reflectors[0].snd(self.reflectors[i])) for i in range(1, 4)]
         print(" - reflections created. awaiting...")
-        print("")
+        print("") 
         return await asyncio.gather(*reflections)
