91d6bd90ca3a5af32085e46a46471d6674f67806
alephpt <alephpt@github.com>
2024-06-23 14:01:58 -0500

    scared

--- Tranception/reflector.py
+++ Tranception/reflector.py
@@ -1,24 +1,43 @@
 from enum import Enum
 
+x = lambda idx, grid_size: idx % grid_size
+y = lambda idx, grid_size: idx % (grid_size * grid_size) // grid_size
+z = lambda idx, grid_size: idx // (grid_size * grid_size)
+unwrap = lambda idx, grid_size: (x(idx, grid_size), y(idx, grid_size), z(idx, grid_size))
+
+def describe(index, grid_size):
+    x, y, z = unwrap(index, grid_size)
+    return f"Index: {index} Cartesian: ({x}, {y}, {z})"
 
 # A Reflection signifies a connection between two reflectors
 class Reflection:
-    def __init__(self, idx, neighbor):
+    def __init__(self, idx, a, b, similance): 
+        # The ID should always be equivalent to A's ID, but may be unique in higher order dimensions, and will break offsets
+        # when wrapping around the grid via toroidal connections
         self.idx = idx
-        self.is_neighbor = neighbor
-        self.induction = 0.0
-        self.interference = 0.0
+        self.a = a
+        self.b = b
+        self.similance = similance   # This determines if the reflection is similar or opposite
+        self.induction = 0.0        # This determines positive or negative flow
+        self.interference = 0.0     # This determines constructive or destructive flow
     
     def __str__(self):
-        return str("Reflection", self.idx)
+        return str("Reflection", describe(self.idx), "Similance: ", self.similance, "Induction: ", self.induction, "Interference: ", self.interference)
+    
+    def __repr__(self):
+        return str(self)
 
 class Reflector:
     def __init__(self, idx):
         self.idx = idx
-        self.reflections = set()  # These are direct neighbors we tend to follow
+        self.reflections = set()  # These are the reflections that are connected to the reflector
+        self.normalize = lambda idx, width: (idx % width, idx // width)
+
+    def addReflection(self, reflection,):
+        self.reflections.add(reflection)
 
     def __str__(self):
-        reflector = str("Reflector {self.idx}")
+        reflector = str("Reflector ", describe(self.idx))
 
         for reflection in self.reflections:
             reflector += f"\n\tNeighbor: {reflection}"
@@ -26,48 +45,33 @@ class Reflector:
             reflector += f"\n\tSimilance: {similance}"
         
         return reflector
-
-normalize_idx = lambda idx, width: (idx % width, idx // width)
-
-# If the node, and the neighbor are in the same row, they are neighbors
-# or, if they are in the same column, they are neighbors, otherwise they oppose
-def isNeighbor(node_idx, neighbor_idx, width):
-    node_col_id, node_row_id = normalize_idx(node_idx, width)
-    neighbor_col_id, neighbor_row_id = normalize_idx(neighbor_idx, width)
+    
+    def __repr__(self):
+        return str(self)
+    
+    # This function could be modified to connect new reflectors and determine their influence
+    def isNeighbor(idx, neighbor_idx, width):
+        # If X,Y || X,Z || Y,Z are the same, then we have a neighbor
+        self_x, self_y, self_z = unwrap(idx, width)
+        neighbor_x, neighbor_y, neighbor_z = unwrap(neighbor_idx, width)
         
-    if node_col_id == neighbor_col_id or node_row_id == neighbor_row_id:
-        return True
-    return False
-    
-# TODO: Implement configuration models where we only look right or up, 
-#       Or we 'square' the grid into a 2D array, into bi-directional connections
-#       or we can cube the grid into a 3D array, into n-dimensional connections
-#       or we can weave it like a mesh, into fully interconnected nodes
+        match_box = 0
+        match_box += 1 if self_x == neighbor_x else 0
+        match_box += 1 if self_y == neighbor_y else 0
+        match_box += 1 if self_z == neighbor_z else 0
 
+        return match_box >= 2 
+
+
+# TODO: Implement this for varying configurations of the grid
 class Dimensionality(Enum):
-    Cellular = 1,
-    Planar = 2,
-    Cubic = 3,
+    Sparse = 1,         # This is does not look angular, but is interconnected e.g. Left, Right, Up, Down, Forward, Backward (not-combinatorially connected)
+    Sparse_Torus = 2,   # This is the 2D network that wraps around itself, nd - circular
+    Cubic = 4,          # This is an interconnected network that is 3D in nature
+    Cubic_Toroidal = 5, # This is the 3D network that wraps around itself in all dimensions, nd - spherical
+    Hyper = 6           # This is a configuration of Every Node connecting to one another
 
-if __name__ == "__main__":
-    dimensions = Dimensionality.Planar
-    grid_size = 3
-    # This constructs a set of reflectors and reflections for a 2x2 grid, without respect to X and Y
-    reflectors = [Reflector(i) for i in range(grid_size * grid_size)]
-    reflections = [] # This is where we determine dimensionality
-
-    index_counter = 0
-    while index_counter < len(reflectors):
-        reflector = reflectors[index_counter]
-
-        if index_counter + 1 > grid_size:
-
-            continue
-    
-
-
-    for reflector in reflectors:
-        print(reflector)
+    __str__ = lambda self: self.name
         
 
 
--- Tranception/tc_engine.py
+++ Tranception/tc_engine.py
@@ -1,13 +1,22 @@
 import asyncio
 import sys
-from tranception import Tranception
+from tranception_new import Tranception
 sys.path.append('../')
 from engine import Engine, Color
 
-class TCEngine(Engine):
-    def __init__(self, screen_size):
-        super().__init__(screen_size, [])
-        self.evokation = Tranception(100)
+# TODO: Abstract a concurent Library for the Engine seperate from the Sampling of the Tranception process.
+#       The game engine is meant to observe at 60hz, while the tranception process is meant to 'sample' at 44.1khz 
+# TODO: Chunk 'prefix' our waveforms to be able to stretch, compress and cache the waveforms
+# TODO: Implement Waveform Analysis and Synthesis as well as color coding
 
-    def activate(self):
-        asyncio.run(self.evokation.induce())
+# This is a wrapper function that interfaces with the game engine through the Engine class
+class TCEngine(Engine):
+    def __init__(self, screen_size, grid_size, dimensions):
+        super().__init__(screen_size, [])
+        self.instance = Tranception(grid_size, dimensions) # Based on Grid Size
+
+    # This allows for concurrent execution of the tranception processes.
+    # This is called every frame of our game loop (60 times per second) ((not 44.1khz!!)) <- This is an entirely different problem
+    def activate(self,):
+        asyncio.run(self.instance.realize())
+
