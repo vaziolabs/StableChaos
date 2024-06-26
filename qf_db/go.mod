90ab6799d74b3d652439436aac96c113e1b63700
alephpt <alephpt@github.com>
2024-06-26 15:37:52 -0500

    branches

--- qf_db/go.mod
+++ qf_db/go.mod
@@ -1,9 +1,11 @@
 module qf_db
 
 replace engine => ./engine
-
 replace _01 => ./_01
 
 go 1.22.4
 
-require engine v0.0.0-00010101000000-000000000000
+require (
+	_01 v0.0.0-00010101000000-000000000000
+	engine v0.0.0-00010101000000-000000000000
+)
--- qf_db/main.go
+++ qf_db/main.go
@@ -1,25 +1,54 @@
 package main
 
 import (
-	"fmt"
-	"engine"
 	"_01"
+	"engine"
 )
 
 func main() {
-	err := engine.InitLogger(engine.Config{
-		Filename:    "log.txt",
-		MinLogLevel: engine.TraceLevel,
-		ToConsole:   true,
-	})
+	// We first want to delete the log file if it exists, but we won't want to do this in production
+	engine.NewFile("log.txt")
+	err := engine.InitLogger(engine.DefaultConfig)
 
 	if err != nil {
-		fmt.Println(err)
+		engine.Log(engine.ErrorLevel, "Error initializing logger: %v", err)
 		return
 	}
 
 	defer engine.Close()
 
-	engine.Log(engine.TraceLevel, " > CompressStringTest")
-	engine.Log(engine.TraceLevel, "Compressed: %s", "String")
+	forest := _01.NewForest("forest")
+	forest.PopulateBranches("health::workout::arms::pushups")
+	forest.PopulateBranches("health::workout::arms::pullups")
+	forest.PopulateBranches("health::workout::core::situps")
+	forest.PopulateBranches("health::workout::lower::squats") // TODO: Fix this where we can have {lower,core, legs}
+	forest.PopulateBranches("health::workout::lower::lunges")
+	forest.PopulateBranches("health::workout::whole::burpees")    // TODO: Fix this where we can have {whole,upper,lower,core,legs,arms}
+	forest.PopulateBranches("health::workout::upper::planks")     // TODO: Fix this where we can have {upper,core,arms}
+	forest.PopulateBranches("health::workout::mountain climbers") //       .. or rather allow for a branch to be linked to multiple other branches e.g. `upper` -> `arms` and `core`
+	forest.PopulateBranches("health::workout::whole::jumping jacks")
+	forest.PopulateBranches("health::workout::lower::flutter kicks")
+
+	forest.PopulateBranches("health::diet::breakfast")
+	forest.PopulateBranches("health::diet::lunch")
+	forest.PopulateBranches("health::diet::dinner")
+	forest.PopulateBranches("health::diet::snacks")
+
+	forest.PopulateBranches("health::mental::meditation")
+	forest.PopulateBranches("health::mental::reading") // This needs to be able to link to education tree {health::mental, education}::reading
+
+	forest.PopulateBranches("education::reading")
+	forest.PopulateBranches("education::writing")
+	forest.PopulateBranches("education::math")
+	forest.PopulateBranches("education::science")
+
+	forest.PopulateBranches("finance::budgeting")
+	forest.PopulateBranches("finance::investing")
+	forest.PopulateBranches("finance::saving")
+
+	forest.PopulateBranches("relationships::family")
+	forest.PopulateBranches("relationships::friends")
+	forest.PopulateBranches("relationships::romantic")
+
+	forest.Log()
 }
